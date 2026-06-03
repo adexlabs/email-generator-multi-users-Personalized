d014a6f188e29e5cfbb537ff7e7a9e1245d67a8c
adexlabs <44570524+adexlabs@users.noreply.github.com>
2026-06-03 12:47:35 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -31,64 +31,24 @@ function renderUsers() {
 
     box.innerHTML = "";
 
-    users.forEach((user, index) => {
+    users.forEach(user => {
 
         box.innerHTML += `
-        <div class="user-item">
-            <label>
-                <input class="user-checkbox" type="checkbox" data-name="${user.name}" data-email="${user.email}">
-                ${user.name}
-                (${user.email})
-            </label>
-        </div>
-        `;
+      <div>
+        <label>
+          <input
+            class="user-checkbox"
+            type="checkbox"
+            data-name="${user.name}"
+            data-email="${user.email}"
+          >
+          ${user.name} (${user.email})
+        </label>
+      </div>
+    `;
     });
+    console.log("User Applied:", users);
 }
-
-// async function sendEmail() {
-
-//     const selectedUsers = [];
-
-//     document
-//         .querySelectorAll("#users input:checked")
-//         .forEach(cb => {
-//             selectedUsers.push(users[cb.value]);
-//         });
-
-//     if (selectedUsers.length === 0) {
-//         alert("Select at least one customer");
-//         return;
-//     }
-
-//     document.getElementById("loader").style.display = "block";
-//     document.getElementById("sendBtn").disabled = true;
-
-//     const response =
-//         await fetch("/send-email", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 users: selectedUsers,
-//                 product:
-//                     document.getElementById("product").value,
-//                 discount:
-//                     document.getElementById("discount").value
-//             })
-//         });
-
-//     const data = await response.json();
-
-//     document.getElementById("loader").style.display = "none";
-//     document.getElementById("sendBtn").disabled = false;
-
-//     document.getElementById("msg").innerText =
-//         data.message ||
-//         data.error ||
-//         "Something went wrong";
-// }
-
 
 async function sendEmail() {
 
@@ -97,73 +57,36 @@ async function sendEmail() {
     document
         .querySelectorAll(".user-checkbox:checked")
         .forEach(cb => {
-
             selectedUsers.push({
                 name: cb.dataset.name,
                 email: cb.dataset.email
+                
             });
-
         });
-
-    // console.log(selectedUsers);
-
-    console.log("Selected Users:", selectedUsers);
-
-    const payload = {
-        users: selectedUsers,
-        product: document.getElementById("product").value,
-        discount: document.getElementById("discount").value
-    };
-
-    console.log("Payload:", payload);
 
     if (selectedUsers.length === 0) {
         alert("Select at least one user");
         return;
     }
 
-    const loader =
-        document.getElementById("loader");
+    const response = await fetch(
+        "/api/send-email",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                users: selectedUsers,
+                product:
+                    document.getElementById("product").value,
+                discount:
+                    document.getElementById("discount").value
+            })
+        }
+    );
 
-    const sendBtn =
-        document.getElementById("sendBtn");
+    const data = await response.json();
 
-    try {
-
-        loader.style.display = "block";
-        sendBtn.disabled = true;
-
-        const response = await fetch(
-            "/api/send-email",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type":
-                        "application/json"
-                },
-                body: JSON.stringify({
-                    users: selectedUsers,
-                    product:
-                        document.getElementById("product").value,
-                    discount:
-                        document.getElementById("discount").value
-                })
-            }
-        );
-
-        const data =
-            await response.json();
-
-        alert(data.message);
-
-    } catch (error) {
-
-        console.log(error);
-        alert("Error sending emails");
-
-    } finally {
-
-        loader.style.display = "none";
-        sendBtn.disabled = false;
-    }
+    alert(data.message);
 }
